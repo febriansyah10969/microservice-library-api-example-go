6993e2b4c1638ee68a35d6935833d3105469d1ac
Febriansyah <febriansyah602@gmail.com>
2023-03-10 14:26:57 +0700

    BEP-30 fix: move uuid to form request

--- internal/dto/transaction.go
+++ internal/dto/transaction.go
@@ -1,6 +1,7 @@
 package dto
 
 type TransactionRequest struct {
-	Qty  int `form:"qty" json:"qty" binding:"required"`
-	Days int `form:"days" json:"days" binding:"required"`
+	BookUUID string `form:"book_uuid" json:"book_uuid" binding:"required"`
+	Qty      int    `form:"qty" json:"qty" binding:"required"`
+	Days     int    `form:"days" json:"days" binding:"required"`
 }
